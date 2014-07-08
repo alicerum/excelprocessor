3825b794c901c3b332adbeb8667320da7b090cc8
i.rum <i.rum@qiwi.ru>
2014-07-08 17:47:34 +0400

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/excelprocessor-standalone.jar clojure.main -m excelprocessor.web
---
+++ test/excelprocessor/web_test.clj
@@ -0,0 +1,6 @@
+(ns excelprocessor.web-test
+  (:require [clojure.test :refer :all]
+            [excelprocessor.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
