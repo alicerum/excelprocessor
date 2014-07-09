3a78f85d029f2b84a65135f58dbc3da67070dc49
i.rum <i.rum@qiwi.ru>
2014-07-09 10:13:15 +0400

    Learning about ring, doing some initial stuff

--- src/excelprocessor/web.clj
+++ src/excelprocessor/web.clj
@@ -1,56 +1,23 @@
 (ns excelprocessor.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
-            [compojure.handler :refer [site]]
-            [compojure.route :as route]
-            [clojure.java.io :as io]
-            [ring.middleware.stacktrace :as trace]
-            [ring.middleware.session :as session]
-            [ring.middleware.session.cookie :as cookie]
             [ring.adapter.jetty :as jetty]
-            [ring.middleware.basic-authentication :as basic]
-            [cemerick.drawbridge :as drawbridge]
+            [ring.middleware.resource :as resource]
+            [ring.middleware.content-type :as ct]
+            [ring.middleware.not-modified :as notmod]
             [environ.core :refer [env]]))
 
-(defn- authenticated? [user pass]
-  ;; TODO: heroku config:add REPL_USER=[...] REPL_PASSWORD=[...]
-  (= [user pass] [(env :repl-user false) (env :repl-password false)]))
 
-(def ^:private drawbridge
-  (-> (drawbridge/ring-handler)
-      (session/wrap-session)
-      (basic/wrap-basic-authentication authenticated?)))
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World!"})
 
-(defroutes app
-  (ANY "/repl" {:as req}
-       (drawbridge req))
-  (GET "/" []
-       {:status 200
-        :headers {"Content-Type" "text/plain"}
-        :body (pr-str ["Hello" :from 'Heroku])})
-  (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
-
-(defn wrap-error-page [handler]
-  (fn [req]
-    (try (handler req)
-         (catch Exception e
-           {:status 500
-            :headers {"Content-Type" "text/html"}
-            :body (slurp (io/resource "500.html"))}))))
-
-(defn wrap-app [app]
-  ;; TODO: heroku config:add SESSION_SECRET=$RANDOM_16_CHARS
-  (let [store (cookie/cookie-store {:key (env :session-secret)})]
-    (-> app
-        ((if (env :production)
-           wrap-error-page
-           trace/wrap-stacktrace))
-        (site {:session {:store store}}))))
+(def app
+  (-> handler
+      (resource/wrap-resource "public")
+      (ct/wrap-content-type)
+      (notmod/wrap-not-modified)))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
-    (jetty/run-jetty (wrap-app #'app) {:port port :join? false})))
-
-;; For interactive development:
-;; (.stop server)
-;; (def server (-main))
+    (jetty/run-jetty app {:port port :join? false})))
